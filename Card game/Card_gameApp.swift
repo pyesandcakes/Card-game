1f6ef026cd5d9a7fd53660dd359cd94a058a3f50
Bruno Simunic <bruno.z.simunic@gmail.com>
2023-03-10 19:02:46 +0100

    Initial Commit

---
+++ Card game/Card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_gameApp.swift
+//  Card game
+//
+//  Created by Bruno on 10.03.2023..
+//
+
+import SwiftUI
+
+@main
+struct Card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
